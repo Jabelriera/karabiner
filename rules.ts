87ac983d3bd652e049c2d42f91732d349d1978a7
Jabel Riera <jabelriera@gmail.com>
2025-02-27 14:00:01 +0100

    More customizations

--- rules.ts
+++ rules.ts
@@ -1,13 +1,79 @@
 import fs from "fs";
-import { KarabinerRules } from "./types";
-import { createHyperSubLayers, app, open, rectangle, shell } from "./utils";
+import {
+  KarabinerRules
+} from "./types";
+import {
+  createHyperSubLayers,
+  app,
+  open,
+  rectangle,
+  shell
+} from "./utils";
 
 const rules: KarabinerRules[] = [
-  // Define the Hyper key itself
+
+  // Fix the problem with < and >
+  {
+    "description": "Swap < and > correctly with Shift",
+    "manipulators": [{
+        "type": "basic",
+        "from": {
+          "key_code": "non_us_backslash",
+          "modifiers": {
+            "optional": ["any"]
+          }
+        },
+        "to": [{
+          "key_code": "grave_accent_and_tilde"
+        }]
+      },
+      {
+        "type": "basic",
+        "from": {
+          "key_code": "non_us_backslash",
+          "modifiers": {
+            "mandatory": ["shift"]
+          }
+        },
+        "to": [{
+          "key_code": "grave_accent_and_tilde",
+          "modifiers": ["shift"]
+        }]
+      },
+      {
+        "type": "basic",
+        "from": {
+          "key_code": "grave_accent_and_tilde",
+          "modifiers": {
+            "optional": ["any"]
+          }
+        },
+        "to": [{
+          "key_code": "non_us_backslash"
+        }]
+      },
+      {
+        "type": "basic",
+        "from": {
+          "key_code": "grave_accent_and_tilde",
+          "modifiers": {
+            "mandatory": ["shift"]
+          }
+        },
+        "to": [{
+          "key_code": "non_us_backslash",
+          "modifiers": ["shift"]
+        }]
+      }
+    ]
+  },
+
+  // ---------------------------------------------------------------- //
+  //      HYPERKEY
+  // ---------------------------------------------------------------- //
   {
     description: "Hyper Key (⌃⌥⇧⌘)",
-    manipulators: [
-      {
+    manipulators: [{
         description: "Caps Lock -> Hyper Key",
         from: {
           key_code: "caps_lock",
@@ -15,27 +81,21 @@ const rules: KarabinerRules[] = [
             optional: ["any"],
           },
         },
-        to: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 1,
-            },
+        to: [{
+          set_variable: {
+            name: "hyper",
+            value: 1,
           },
-        ],
-        to_after_key_up: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 0,
-            },
+        }, ],
+        to_after_key_up: [{
+          set_variable: {
+            name: "hyper",
+            value: 0,
           },
-        ],
-        to_if_alone: [
-          {
-            key_code: "escape",
-          },
-        ],
+        }, ],
+        to_if_alone: [{
+          key_code: "escape",
+        }, ],
         type: "basic",
       },
       //      {
@@ -56,10 +116,19 @@ const rules: KarabinerRules[] = [
     ],
   },
   ...createHyperSubLayers({
-    spacebar: open(
-      "raycast://extensions/HenriChabrand/notion/create-database-page"
-    ),
-    // b = Browse
+
+    spacebar: {
+
+      to: [{
+        key_code: "p",
+        modifiers: ["left_command", "left_control"]
+      }],
+    },
+
+    // ---------------------------------------------------------------- //
+    // B = Brave / Browse
+    // ---------------------------------------------------------------- //
+
     b: {
 
       spacebar: open("https://pdpaola.com"),
@@ -67,9 +136,18 @@ const rules: KarabinerRules[] = [
       m: open("https://mail.google.com"),
       x: open("https://x.com"),
 
+      d: {
+        to: [{
+          key_code: "d",
+          modifiers: ["left_command", "left_option"]
+        }],
+      },
     },
 
-    // j = Jira
+    // ---------------------------------------------------------------- //
+    // J = Jira
+    // ---------------------------------------------------------------- //
+
     j: {
 
       spacebar: open("raycast://extensions/thomaslombart/jira/my-filters"),
@@ -86,11 +164,22 @@ const rules: KarabinerRules[] = [
       w: /*  COM  */ open("https://pdpaola.atlassian.net/jira/software/c/projects/COM/boards/46"),
       e: /*  DAT  */ open("https://pdpaola.atlassian.net/jira/software/c/projects/DAT/boards/45"),
 
-      x: open("raycast://quicklink?name=View Ticket"),
+      v: {
+        to: [{
+          key_code: "v",
+          modifiers: ["left_command", "left_option"]
+        }],
+      },
     },
 
     // o = "Open" applications
     a: {
+      spacebar: {
+        to: [{
+          key_code: "tab",
+          modifiers: ["left_command", "left_option"],
+        }, ],
+      },
       b: app("Brave Browser"),
       c: app("Notion Calendar"),
       d: app("Discord"),
@@ -100,13 +189,34 @@ const rules: KarabinerRules[] = [
       1: app("1Password"),
       s: app("Slack"),
       t: app("Terminal"),
+      v: app("Visual Studio Code"),
       w: app("Whatsapp"),
     },
 
-    // n = Notion
+    i: {
+      a: app("ChatGPT"),
+    },
+
+    // ---------------------------------------------------------------- //
+    // N = Notion
+    // ---------------------------------------------------------------- //
+    
     n: {
 
-      spacebar: open("notion://www.notion.so/jriera/Follow-up-Dpts-cc2d07d307174663aeee0192029d4ee5?pvs=4"),
+      // ---------------- //
+      // Quick Search     //
+      // ---------------- //
+
+      spacebar:
+        /*{
+               to: [{ key_code: "p", modifiers: ["left_command","left_control"] }],
+             },*/
+        open("notion://www.notion.so/jriera/Babylon-5286b76c5f814b5e80018c9c2f1746e5?pvs=25"),
+
+
+      // ---------------- //
+      // Follow-up Pages  //
+      // ---------------- //
 
       q: /*  BAB  */ open("notion://www.notion.so/jriera/Babylon-5286b76c5f814b5e80018c9c2f1746e5?pvs=25"),
       w: /*  COM  */ open("notion://www.notion.so/jriera/Ecommerce-4d14585900d14482a6663b38e24c1460?pvs=25"),
@@ -115,11 +225,51 @@ const rules: KarabinerRules[] = [
 
       a: /*  CEO  */ open("notion://www.notion.so/jriera/Humbert-421761737c744a8e95065153c18fed63?pvs=4"),
       s: /* TODOs */ open("notion://www.notion.so/jriera/19e9bb54bc0380d7bf9ee11350cc13d7?v=19e9bb54bc0380c4899d000c6d019498&pvs=4"),
+      d: /* Dashboard */ open("notion://www.notion.so/jriera/Control-Center-cc2d07d307174663aeee0192029d4ee5?pvs=4"),
+
+
+      // ---------------- //
+      // Add to Databases //
+      // ---------------- //
+
+      i: {
+        // Manually add to To Do List
+        to: [{
+          key_code: "i",
+          modifiers: ["left_command", "left_option"]
+        }],
+      },
+
+      o: {
+        // Manually add to Read Later List
+        to: [{
+          key_code: "o",
+          modifiers: ["left_command", "left_option"]
+        }],
+      },
+
+      k: {
+        // Add to To Do List
+        to: [{
+          key_code: "k",
+          modifiers: ["left_command", "left_option"]
+        }],
+      },
+
+      l: {
+        // Add to Read Later List
+        to: [{
+          key_code: "l",
+          modifiers: ["left_command", "left_option"]
+        }],
+      },
 
     },
 
-    // w = "Window" via rectangle.app IT-5514
-    /*
+    // ---------------------------------------------------------------- //
+    // W = Windows Management
+    // ---------------------------------------------------------------- //
+
     w: {
       semicolon: {
         description: "Window: Hide",
@@ -179,59 +329,48 @@ const rules: KarabinerRules[] = [
       f: {
         to: [{ shell_command: "osascript -e 'tell application \"System Events\" to keystroke \"f\" using {command down, control down}'" }],
       },
-    },*/
+    },
 
-    // s = "System"
+    // ---------------------------------------------------------------- //
+    // S = System
+    // ---------------------------------------------------------------- //
+
     s: {
       u: {
-        to: [
-          {
-            key_code: "volume_increment",
-          },
-        ],
+        to: [{
+          key_code: "volume_increment",
+        }, ],
       },
       j: {
-        to: [
-          {
-            key_code: "volume_decrement",
-          },
-        ],
+        to: [{
+          key_code: "volume_decrement",
+        }, ],
       },
       i: {
-        to: [
-          {
-            key_code: "display_brightness_increment",
-          },
-        ],
+        to: [{
+          key_code: "display_brightness_increment",
+        }, ],
       },
       k: {
-        to: [
-          {
-            key_code: "display_brightness_decrement",
-          },
-        ],
+        to: [{
+          key_code: "display_brightness_decrement",
+        }, ],
       },
       l: {
-        to: [
-          {
-            key_code: "q",
-            modifiers: ["right_control", "right_command"],
-          },
-        ],
+        to: [{
+          key_code: "q",
+          modifiers: ["right_control", "right_command"],
+        }, ],
       },
       p: {
-        to: [
-          {
-            key_code: "play_or_pause",
-          },
-        ],
+        to: [{
+          key_code: "play_or_pause",
+        }, ],
       },
       semicolon: {
-        to: [
-          {
-            key_code: "fastforward",
-          },
-        ],
+        to: [{
+          key_code: "fastforward",
+        }, ],
       },
 
       // "D"o not disturb toggle
@@ -243,67 +382,98 @@ const rules: KarabinerRules[] = [
       c: open("raycast://extensions/raycast/system/open-camera"),
       // 'v'oice
       v: {
-        to: [
-          {
-            key_code: "spacebar",
-            modifiers: ["left_option"],
-          },
-        ],
+        to: [{
+          key_code: "spacebar",
+          modifiers: ["left_option"],
+        }, ],
       },
     },
 
     // v = "moVe" which isn't "m" because we want it to be on the left hand
-    // so that hjkl work like they do in vim
+    // so that hjkl work like the
+    // y do in vim
     v: {
       h: {
-        to: [{ key_code: "left_arrow" }],
+        to: [{
+          key_code: "left_arrow"
+        }],
       },
       j: {
-        to: [{ key_code: "down_arrow" }],
+        to: [{
+          key_code: "down_arrow"
+        }],
       },
       k: {
-        to: [{ key_code: "up_arrow" }],
+        to: [{
+          key_code: "up_arrow"
+        }],
       },
       l: {
-        to: [{ key_code: "right_arrow" }],
+        to: [{
+          key_code: "right_arrow"
+        }],
       },
       // Magicmove via homerow.app
       m: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
+        to: [{
+          key_code: "f",
+          modifiers: ["right_control"]
+        }],
         // TODO: Trigger Vim Easymotion when VSCode is focused
       },
       // Scroll mode via homerow.app
       s: {
-        to: [{ key_code: "j", modifiers: ["right_control"] }],
+        to: [{
+          key_code: "j",
+          modifiers: ["right_control"]
+        }],
       },
       d: {
-        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
+        to: [{
+          key_code: "d",
+          modifiers: ["right_shift", "right_command"]
+        }],
       },
       u: {
-        to: [{ key_code: "page_down" }],
+        to: [{
+          key_code: "page_down"
+        }],
       },
       i: {
-        to: [{ key_code: "page_up" }],
+        to: [{
+          key_code: "page_up"
+        }],
       },
       f: {
-        to: [{ shell_command: "osascript -e 'tell application \"System Events\" to keystroke \"f\" using {command down, control down}'" }],
+        to: [{
+          shell_command: "osascript -e 'tell application \"System Events\" to keystroke \"f\" using {command down, control down}'"
+        }],
       },
     },
 
     // c = Musi*c* which isn't "m" because we want it to be on the left hand
     c: {
       p: {
-        to: [{ key_code: "play_or_pause" }],
+        to: [{
+          key_code: "play_or_pause"
+        }],
       },
       n: {
-        to: [{ key_code: "fastforward" }],
+        to: [{
+          key_code: "fastforward"
+        }],
       },
       b: {
-        to: [{ key_code: "rewind" }],
+        to: [{
+          key_code: "rewind"
+        }],
       },
     },
 
-    // r = "Raycast"
+    // ---------------------------------------------------------------- //
+    // R = Raycast
+    // ---------------------------------------------------------------- //
+
     r: {
       c: open("raycast://extensions/thomas/color-picker/pick-color"),
       g: open("raycast://extensions/josephschmitt/gif-search/search"),
@@ -329,26 +499,23 @@ const rules: KarabinerRules[] = [
 
 fs.writeFileSync(
   "karabiner.json",
-  JSON.stringify(
-    {
+  JSON.stringify({
       global: {
-        show_in_menu_bar: true,
+        show_in_menu_bar: false,
         keyboard_type: "iso"
       },
-      profiles: [
-        {
-          name: "Default",
-          keyboard_type: "iso",
-          selected: true,
-          virtual_hid_keyboard: {
-            country_code: 0,
-            keyboard_type_v2: "iso"
-          },
-          complex_modifications: {
-            rules,
-          },
+      profiles: [{
+        name: "Jabel",
+        keyboard_type: "iso",
+        selected: true,
+        virtual_hid_keyboard: {
+          country_code: 0,
+          keyboard_type_v2: "iso"
         },
-      ],
+        complex_modifications: {
+          rules,
+        },
+      }, ],
     },
     null,
     2
